f1e4cc30357e764111398de44e0a9a456f22d8a6
emilybonar <emilykbonar@gmail.com>
2021-02-13 16:10:47 -0600

    Add better icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ function MainScreen() {
 
 function PersonCard() {
 	return (
-		<div className="z-10 flex flex-col w-5/6 max-w-lg overflow-hidden bg-gray-200 border border-gray-400 rounded-lg shadow-lg md:w-2/3 h-3/4">
+		<div className="z-10 flex flex-col w-5/6 max-w-lg overflow-hidden bg-gray-200 border border-gray-300 rounded-lg shadow-lg md:w-2/3 h-3/4">
 			<img
 				className="object-contain object-center h-2/3"
 				src="https://www.thispersondoesnotexist.com/image"
@@ -74,9 +74,38 @@ function PersonCard() {
 					</div>
 				</div>
 			</main>
-			<div className="flex flex-row justify-between text-center text-white text-8xl">
-				<div className="w-56 h-56 bg-gray-500 rounded-tr-3xl">X</div>
-				<div className="w-56 h-56 bg-red-500 rounded-tl-3xl">{"<3"}</div>
+			<div className="flex flex-row justify-between h-full text-center text-white text-8xl">
+				<div className="grid w-56 bg-gray-500 rounded-tr-3xl place-items-center">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="96"
+						height="96"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				</div>
+				<div className="grid w-56 bg-red-500 rounded-tl-3xl place-items-center">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="96"
+						height="96"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+					</svg>
+				</div>
 			</div>
 		</div>
 	);
